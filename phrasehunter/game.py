21c8207d56acb2009a8f58f0eb8ce5f33c65c169
arnavpatel82 <arnav.patel82@yahoo.com>
2022-05-28 23:13:54 +0800

    Game class with start() method incomplete

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -13,16 +13,28 @@ class Game:
     def get_random_phrase(self):
         return random.choice(self.phrases)
 
-    def get_guess(self, guess):
+    def get_guess(self):
+        while True:
+            guess = input("Guess a letter. ")
+            if guess.isalpha() and len(guess) == 1:
+                break
+            else:
+                print("Your guess must be a letter of the alphabet")
+
         self.guesses.append(guess)
 
     def welcome(self):
         print("============================")
         print("Welcome to the Phrase Hunter Game")
         print("============================")
+        print()
     
-    def game_over(self):
-        pass
+    def game_over(self, win):
+        if win:
+            print("Congratulations you won!")
+        else:
+            print("Game over! Better luck next time!")
 
     def start(self):
-        pass
+        self.welcome()
+        
